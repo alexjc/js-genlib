37691c1fa388f0802fed596ba402af137f65d341
Alex J. Champandard <445208+alexjc@users.noreply.github.com>
2019-04-30 11:34:09 +0200

    📋 Schema to define skill inputs and outputs.

---
+++ genlib/core/schema.ts
@@ -0,0 +1,14 @@
+export class SkillOutput {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+};
+  
+export class SkillInput {
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+};
