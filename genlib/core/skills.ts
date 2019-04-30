56c4490ab8ceaeff35691aedc494d5b6007ba7c8
Alex J. Champandard <445208+alexjc@users.noreply.github.com>
2019-04-30 13:14:34 +0200

    👋 Derived skills can specify their own inputs and outputs.

--- genlib/core/skills.ts
+++ genlib/core/skills.ts
@@ -3,6 +3,16 @@ import { SkillInput, SkillOutput } from './schema';
 
 class BaseSkill {
 
+  static inputs = [];
+  static outputs = [];
+
+  get_inputs() {
+    return (<typeof BaseSkill> this.constructor).inputs;
+  }
+
+  get_outputs() {
+    return (<typeof BaseSkill> this.constructor).outputs;
+  }
 };
 
 export { SkillOutput as Output, SkillInput as Input, BaseSkill as Skill };
--- tests/core/skills.unit.ts
+++ tests/core/skills.unit.ts
@@ -1,7 +1,39 @@
-import { Skill } from '../../genlib/core/skills';
+import { Skill, Input, Output } from '../../genlib/core/skills';
 
-describe("custom skill", () => {
+describe("base skill", () => {
   it("builds", () => {
     expect(new Skill()).toBeTruthy();
+  });
+
+  it("has no outputs", () => {
+    var skill = new Skill();
+    expect(skill.get_outputs().length).toBe(0);
+  });
+
+  it("has no inputs", () => {
+    var skill = new Skill();
+    expect(skill.get_inputs().length).toBe(0);
+  })
+});
+
+describe("custom skill", () => {
+  it("can define an output", () => {
+    class FakeSkill extends Skill {
+      static outputs: Output[] = [
+        new Output("efgh")
+      ];
+    };
+    var skill = new FakeSkill();
+    expect(skill.get_outputs().length).toBe(1);
+  });
+
+  it("can define an input", () => {
+    class FakeSkill extends Skill {
+      static inputs: Input[] = [
+        new Input("efgh")
+      ];
+    };
+    var skill = new FakeSkill();
+    expect(skill.get_inputs().length).toBe(1);
   });
 });
